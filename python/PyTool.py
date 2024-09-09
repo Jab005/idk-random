6da947753f58f277acbfc47ce7d825d7521ba8c3
Jab <164631211+Jab005@users.noreply.github.com>
2024-09-09 19:15:07 +0700

    Update
    Ran out of idea..

--- python/PyTool.py
+++ python/PyTool.py
@@ -22,7 +22,7 @@ class LogicGate:
 #Some extra utilities
 class Util:
   
-  #User just need to do 'Util()' and they'll be hinted with another help method
+  #User just need to do 'Util()' and they'll be hinted with another help methods
   def __init__(self, method:str=""):
     selection = method.lower()
     match selection:
@@ -31,13 +31,13 @@ class Util:
       case "between":
         print("Return boolean of value in between minimum and maximum value.\nUsage: between(minVal:float|int, maxVal:float|int, value:float|int)")
       case "point2da":
-        print("Visualizes 2 dimensional coordinate map with pointer. Start from one at top-left to bottom-right. Inputting any pointer to zero will return no pointer.\nUsage: point2DA(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
+        print("Visualizes 2 dimensional coordinate map with pointer. Start from one at top-left to bottom-right. Inputting any pointer to zero or lower will return no pointer.\nUsage: point2DA(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
       case "point2db":
-        print("Visualizes 2 dimensional coordinate map with pointer. Start from one at bottom-left to top-right. Inputting any pointer to zero will return no pointer.\nUsage: point2DB(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
+        print("Visualizes 2 dimensional coordinate map with pointer. Start from one at bottom-left to top-right. Inputting any pointer to zero or lower will return no pointer.\nUsage: point2DB(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
       case "point2dc":
-        print("Visualizes 2 dimensional coordinate map with pointer. Start from zero at top-left to bottom-right.\nUsage: point2DC(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
+        print("Visualizes 2 dimensional coordinate map with pointer. Start from zero at top-left to bottom-right. Inputting locationX or locationY lower than zero will return no point.\nUsage: point2DC(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
       case "point2dd":
-        print("Visualizes 2 dimensional coordinate map with pointer. Start from zero at bottom-left to top-right.\nUsage: point2DD(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
+        print("Visualizes 2 dimensional coordinate map with pointer. Start from zero at bottom-left to top-right. Inputting locationX or locationY lower than zero will return no point.\nUsage: point2DD(locationX:int, locationY:int, maxX:int, maxY:int, point:str='X', empty:str='o')")
       case "formattext":
         print("Formatting multiple symbols to string array. Any excess of symbols in string (not enough placeholder) will return last placeholder for next symbols.\nUsage: formatText(text:str, symbol:str='@',*placeholder:str)")
       case "factorial":
@@ -46,6 +46,8 @@ class Util:
         print("Return the result of number tetration. May not working well when the result is too big.\nUsage: tetra(num:int|float, mult:int)")
       case "strremove":
         print("Removes string parts from string.\nUsage: strRemove(text:str, *part:str)")
+      case "rate":
+        print("Calculates growth rate of starting number by its percentage for amount of loop times.\nUsage: rate(num:int|float=0,percent:int=0,loop:int=1)")
       case _:
         print("Extra utilities for python.\nDo 'Utilities('method')' for each method info.\nAvailable methods:\n1. sqrt\n2. between\n3. point2DA\n4. point2DB\n5. point2DC\n6. point2DD\n7. formatText\n8. factorial\n9. tetra\n10. strRemove")
   
@@ -56,11 +58,11 @@ class Util:
     else:
       return value**0.5
   
-  #Return True if given value is greater or equal of min value and less or equal of max value (shortcut of double and statements). Useful if you're trying to input multi random chance without creating a variable
+  #Return True if given value is greater or equal of min value and less or equal of max value (shortcut of double and in between numbers statement). Useful if you're trying to input multi random chance without creating a variable
   def between(minVal:float|int, maxVal:float|int, value:float|int):
     return value >= minVal and value <= maxVal
   
-  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from one at top-left to bottom-right. Inputting locationX or locationY zero will return no point
+  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from one at top-left to bottom-right. Inputting locationX or locationY zero or lower will return no point
   def point2DA(locationX:int, locationY:int, maxX:int, maxY:int, point:str="X", empty:str="o"):
     for posX in range(maxY):
       notation = ""
@@ -71,7 +73,7 @@ class Util:
           notation += empty
       print(notation)
   
-  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from one at bottom-left to top-right. Inputting locationX or locationY zero will return no point
+  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from one at bottom-left to top-right. Inputting locationX or locationY zero or lower will return no point
   def point2DB(locationX:int, locationY:int, maxX:int, maxY:int, point:str="X", empty:str="o"):
     for posX in range(maxY):
       notation = ""
@@ -82,7 +84,7 @@ class Util:
           notation += empty
       print(notation)
   
-  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from zero at top-left to bottom-right
+  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from zero at top-left to bottom-right. Inputting locationX or locationY lower than zero will return no point
   def point2DC(locationX:int, locationY:int, maxX:int, maxY:int, point:str="X", empty:str="o"):
     for posX in range(maxY):
       notation = ""
@@ -93,7 +95,7 @@ class Util:
           notation += empty
       print(notation)
   
-  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from zero at bottom-left to top-right
+  #Create simple dot mapping visualization of XY coordinate position in 2D. Start from zero at bottom-left to top-right. Inputting locationX or locationY lower than zero will return no point
   def point2DD(locationX:int, locationY:int, maxX:int, maxY:int, point:str="X", empty:str="o"):
     for posX in range(maxY):
       notation = ""
@@ -134,3 +136,10 @@ class Util:
       resl = resl.replace(part[min(rep,len(part)-1)],"")
     return resl
   
+  #Calculates growth rate of starting number by its percentage for amount of loop times
+  def rate(num:int|float=0,percent:int=0,loop:int=1):
+    val = num
+    for gl in range(loop):
+      val += val*(percent/100)
+    return val
+
